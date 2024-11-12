8281d838627e3bdb502252bb9fe0136c57cfa4d5
davidhadinugroho <davidhadinugroho24601@gmail.com>
2024-11-12 12:01:13 +0800

    adding bootstrap locally

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,19 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Document</title>
+    
+        <title>Document</title>
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="/bs-css/bootstrap.css" rel="stylesheet">
+    <script src="/bs-js/bootstrap.bundle.min.js"></script>
+
 </head>
 <body>
 <div class="container">
     <header class="d-flex justify-content-center py-3">
+      
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-        <li class="nav-item"><a style="color:#005293" href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a style="color:#005293" href="#" class="nav-link">About us</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Categories</a></li>
+        <li class="nav-item" ><a href="#" class="nav-link">Contact</a></li>
+        <li class="nav-item" ><a href="#" class="nav-link">Contact</a></li>
+        <li class="nav-item " ><a href="#" class="btn btn-primary">login</a> <a href="#" class="btn btn-light">logout</a></li>
+        
+        <!-- <li class="nav-item"><a href="#" class="nav-link">About</a></li> -->
       </ul>
     </header>
+
   </div>
