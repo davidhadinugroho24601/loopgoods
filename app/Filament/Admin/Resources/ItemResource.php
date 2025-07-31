22bd20291f1a03741a4f695af632db5f796b516d
davidhadinugroho <davidhadinugroho24601@gmail.com>
2025-07-31 21:09:27 +0800

    commit

--- app/Filament/Admin/Resources/ItemResource.php
+++ app/Filament/Admin/Resources/ItemResource.php
@@ -124,7 +124,7 @@ class ItemResource extends Resource
         return $table
             ->columns([
                 TextColumn::make('name')->searchable()->sortable(),
-                TextColumn::make('category.name')->label('Category')->sortable(),
+                TextColumn::make('category.name')->searchable()->label('Category')->sortable(),
                 TextColumn::make('quantity')->label('Quantity'),
                 TextColumn::make('max_request')->label('Maximum Request'),
                 TextColumn::make('stock')->label('Available Stock'),
